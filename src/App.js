f5ecae4a66dc2e0a1bfdfdc27dc2a040694ff990
bilal05476 <ba4762313@gmail.com>
2023-11-05 14:56:18 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import CodeBlock from "./CodeBlock";
 import Objective from "./Objective";
-import { steps } from "./steps";
 
 function App() {
   return (
@@ -11,27 +9,11 @@ function App() {
           textAlign: "center",
         }}
       >
-        🚀 Project 2: Jenkins Declarative CI/CD Pipeline on AWS via with Docker
-        Compose and Web Hook.
+        🚀 Project 2: Jenkins Declarative CI/CD Pipeline on AWS via Docker
+        Compose and WebHook.
       </h2>
       <Objective />
-      <h2
-        style={{
-          marginBottom: "1rem",
-        }}
-      >
-        Step by Step Impelementation
-      </h2>
-      {steps.map((item, ind) => (
-        <CodeBlock
-          key={ind}
-          indx={ind}
-          desc={item.desc}
-          img={item.img}
-          code={item.code}
-          text={item.text}
-        />
-      ))}
+
       <h4
         style={{
           marginBottom: "1rem",
--- src/Objective.jsx
+++ src/Objective.jsx
@@ -6,51 +6,51 @@ const Objective = () => {
       <BlockofContent
         heading={"Project Objectives:"}
         content={[
-          "Deploy two Docker containers using Ansible.",
-          "One container should run an Apache service with a static web page.",
-          "The other container should run an FTP service.",
-          "Configure the containers to allow network communication.",
-          "Verify accessibility from the host machine.",
+          "Set up Jenkins to automate the build and deployment process.",
+          "Implement a Jenkins Declarative Pipeline for defining the CI/CD process.",
+          "Use Docker Compose to define and manage the application's containers.",
+          "Deploy the application on AWS infrastructure.",
+          "Set up a webhook to trigger the CI/CD pipeline on code changes.",
         ]}
       />
       <BlockofContent
         heading={"Requirements:"}
         content={[
-          "Docker installed on the target machine(s).",
-          "Ansible installed on the control machine.",
+          "Install and configure Jenkins on a server.",
+          "Install Docker and Docker Compose on the Jenkins server.",
+          "Launch an AWS service with the necessary key and security (EC2).",
+          "Host your code in a Git repository (GitHub).",
         ]}
       />
       <BlockofContent
         heading={"Project Configurations:"}
         content={[
-          "*Apache Container Configuration:*",
-          "Docker image: `httpd:latest`",
-          "Static web page location: `/home/ubuntu/static/index.html`",
-          "Container should restart automatically on port 80",
-          "*FTP Container Configuration:*",
-          "Docker image: `fauria/vsftpd`",
-          "Container should restart automatically. - Port 21 exposed.",
-          "*Network Configuration:*",
-          "Containers should be on different subnets.",
-          "Network diagram required to depict containers running on different subnets but reachable from the host machine.",
+          "*Jenkins Configuration:*",
+          "Configure Jenkins to connect to your Git repository.",
+          "Set up Jenkins credentials for Docker hub (username and password).",
+          "Install necessary plugins (Docker, AWS, Pipeline).",
+
+          "*Docker Compose Configuration:*",
+          "Write a docker-compose.yml file for your application's services.",
+
+          "*AWS Configuration:*",
+          "Create an EC2 instance for hosting Jenkins and Docker containers.",
+
+          "*Webhook Configuration:*",
+          "Set up a webhook in your Git repository to trigger Jenkins on code changes.",
         ]}
       />
       <BlockofContent
         heading={"Tasks:"}
         content={[
-          "Deploy Apache container: - Copy static web content. - Ensure the container restarts automatically.",
-          "Deploy FTP container: - Configure FTP service.",
-          "Verify network communication: - Ensure containers are reachable from the host machine.",
-          "Create a network diagram: - Depict containers running on different subnets.",
-        ]}
-      />
-      <BlockofContent
-        heading={"Additional Configuration Details:"}
-        content={[
-          "AWS EC2 instances are used.",
-          "Apache container serves content on port 80.",
-          "FTP container runs on port 21.",
-          "Docker containers should restart automatically on instance reboot.",
+          "Jenkins Pipeline Script: - Write a Jenkins Declarative Pipeline script (Jenkinsfile) in your repository. - Define stages for clone, build, push, and deployment.",
+          "Clone Github Code: - Clones the code from the specified GitHub repository and branch.",
+          "Docker Image Build: - Use Dockerfile to define the build image for your code.",
+          "Docker Image Push: - Tags the built Docker image, logs in to Docker Hub using credentials, and pushes the image to the repository.",
+          "Docker Container Deploy: - Deploys the containerized application using Docker Compose, taking down existing containers and bringing up the new ones in detached mode.",
+          "Webhook Trigger: - est the webhook by changing your Git repository. - Ensure the Jenkins pipeline is triggered automatically.",
+          "Pipeline Validation: - Validate the entire pipeline by changing your code and observing the pipeline stages.",
+          "Security Considerations: - Ensure your Docker hub credentials in Jenkins are securely managed. - Implement appropriate security measures for your Docker hub.",
         ]}
       />
     </div>
@@ -63,7 +63,7 @@ const BlockofContent = ({ heading, content }) => {
       <h4>{heading}</h4>
       <ol>
         {content.map((item) => (
-          <li>{item}</li>
+          <li className={item.startsWith("*") && "underline"}>{item}</li>
         ))}
       </ol>
     </div>
